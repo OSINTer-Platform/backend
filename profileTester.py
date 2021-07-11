c13d4c24bf02a33747216a03b6f6840359bb9acf
bertmad3400 <skrivtilbertram@gmail.com>
2021-07-11 15:24:10 +0200

    Add testing script for profiles
    Will take a profile and scrape some articles based on it, allowing you
    as the the developer to easily verify that the profiles are targeting
    the right parts of the page. IS NOT FOR USE IN PRODUCTION OF ANY SORT,
    but only for development of profiles.

---
+++ profileTester.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+# The name of your obsidian vault
+obsidianVault = ""
+# The absolute path to your obsidian vault
+vaultPath = ""
+
+
+import os
+
+# Mainly used for sleeping
+import time
+
+# The profiles mapping the different websites are in json format
+import json
+
+# Used for detecting when the user closes the web driver
+import selenium
+
+from pathlib import Path
+
+debugMessages = True
+
+from OSINTmodules.OSINTprofiles import getProfiles
+from OSINTmodules import *
+
+def handleSingleArticle(vaultName, vaultPath, profileName, articleSource, articleURL):
+
+    OSINTlocal.openURILocally(articleURL)
+
+    # Load the profile for the article
+    currentProfile = json.loads(getProfiles(profileName))
+
+    # Gather the needed information from the article
+    articleDetails, articleContent, articleClearText = OSINTextract.extractAllDetails(currentProfile, articleSource)
+
+    # Generate the tags
+    articleTags = OSINTtext.generateTags(OSINTtext.cleanText(articleClearText))
+
+    # Create the markdown file
+    MDFileName = OSINTfiles.createMDFile(currentProfile['source']['name'], articleURL, articleDetails, articleContent, articleTags)
+    os.rename(Path(MDFileName).resolve(), Path(vaultPath + MDFileName))
+
+
+profile = input("Which profile do you want to test? ")
+articleURLLists = OSINTscraping.gatherArticleURLs([getProfiles(profile)])
+
+for URLlist in articleURLLists:
+    currentProfile = URLlist.pop(0)
+    for url in URLlist:
+        OSINTmisc.printDebug("Scraping: " + url)
+        articleSource = OSINTscraping.scrapePageDynamic(url)
+        handleSingleArticle(obsidianVault, vaultPath, currentProfile, articleSource, url)
