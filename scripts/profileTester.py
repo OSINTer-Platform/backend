e876593ff3a2998f3f2251831a015811ef919bf2
bertmad3400 <skrivtilbertram@gmail.com>
2021-08-18 17:07:18 +0200

    Updated profiletester to work with cleaned up OSINTmodules

--- scripts/profileTester.py
+++ scripts/profileTester.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 # The name of your obsidian vault
-obsidianVault = "Testing"
+obsidianVault = ""
 # The absolute path to your obsidian vault
-vaultPath = "/home/bertmad/Obsidian/Testing/"
+vaultPath = ""
 
+# For recognize the os
+import sys
 
 import os
 
@@ -24,9 +26,31 @@ debugMessages = True
 from OSINTmodules.OSINTprofiles import getProfiles
 from OSINTmodules import *
 
+# Function checking whether the variables specifying which obsidian vault to use is set
+def checkIfObsidianDetailsSet():
+    if obsidianVault == "" or vaultPath == "":
+        raise Exception("You need to specify which Obsidian vault to use, and the path to it. These details are defined in the very part of the script as variables.")
+
+def openURILocally(URI):
+    # And lastly open the file in obsidian by using an URI along with the open command for the currently used OS
+    platform = sys.platform
+
+    if platform.startswith('linux'):
+        openCommand = "xdg-open '" + URI + "'"
+    elif platform.startswith('win32'):
+        openCommand = "start '" + URI + "'"
+    elif platform.startswith('darwin'):
+        openCommand = "open '" + URI + "'"
+    elif platform.startswith('cygwin'):
+        openCommand = "cygstart '" + URI + "'"
+    else:
+        raise Exception("Unfortunatly, your system isn't support. You should be running a new version of Windows, Mac or Linux.")
+
+    os.system(openCommand)
+
 def handleSingleArticle(vaultName, vaultPath, profileName, articleSource, articleURL):
 
-    OSINTlocal.openURILocally(articleURL)
+    openURILocally(articleURL)
 
     # Load the profile for the article
     currentProfile = json.loads(getProfiles(profileName))
@@ -39,10 +63,11 @@ def handleSingleArticle(vaultName, vaultPath, profileName, articleSource, articl
 
     # Create the markdown file
     MDFileName = OSINTfiles.createMDFile(currentProfile['source']['name'], articleURL, articleDetails, articleContent, articleTags)
-    os.rename(Path(MDFileName).resolve(), Path(vaultPath + MDFileName))
+    os.rename(str(Path(MDFileName).resolve()) + ".md", str(Path(vaultPath + MDFileName)) + ".md")
 
 
 def main():
+    checkIfObsidianDetailsSet()
     profile = input("Which profile do you want to test? ")
     articleURLLists = OSINTscraping.gatherArticleURLs([getProfiles(profile)])
 
