fc92ff98599f2ac582d8f8ee701cc39e1f67eb55
bertmad3400 <skrivtilbertram@gmail.com>
2021-10-06 10:41:44 +0200

    Add catch if folder for credentials, articles and logs exists already

--- scripts/initiateScrapingBackend.py
+++ scripts/initiateScrapingBackend.py
@@ -29,6 +29,11 @@ def createFolder(folderName):
         except:
             # This shoudln't ever be reached, as it would imply that the folder doesn't exist, but the script also is unable to create it. Could possibly be missing read permissions if the scripts catches this exception
             raise Exception("The folder {} couldn't be created, exiting".format(folderName))
+    else:
+        try:
+            os.chmod(Path("./" + folderName), 0o750)
+        except:
+            raise Exception("Failed to set the 750 permissions on {}, either remove the folder or set the right perms yourself and try again.".format(folderName))
 
 # Mozilla will have an api endpoint giving a lot of information about the latest releases for the geckodriver, from which the url for the linux 64 bit has to be extracted
 def extractDriverURL():
