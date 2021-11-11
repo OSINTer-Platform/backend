17c0da28bdb0b50ae36191a602afeb8431c325a8
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-11 10:48:58 +0100

    Update profileTester to work with new OSINTmodules and scrapeAndStore

--- scripts/profileTester.py
+++ scripts/profileTester.py
@@ -26,57 +26,23 @@ debugMessages = True
 from OSINTmodules.OSINTprofiles import getProfiles
 from OSINTmodules import *
 
+from scripts.scrapeAndStore import scrapeUsingProfile
+
 # Function checking whether the variables specifying which obsidian vault to use is set
 def checkIfObsidianDetailsSet():
     if obsidianVault == "" or vaultPath == "":
         raise Exception("You need to specify which Obsidian vault to use, and the path to it. These details are defined in the very part of the script as variables.")
-
-def openURILocally(URI):
-    # And lastly open the file in obsidian by using an URI along with the open command for the currently used OS
-    platform = sys.platform
-
-    if platform.startswith('linux'):
-        openCommand = "xdg-open '" + URI + "'"
-    elif platform.startswith('win32'):
-        openCommand = "start '" + URI + "'"
-    elif platform.startswith('darwin'):
-        openCommand = "open '" + URI + "'"
-    elif platform.startswith('cygwin'):
-        openCommand = "cygstart '" + URI + "'"
-    else:
-        raise Exception("Unfortunatly, your system isn't support. You should be running a new version of Windows, Mac or Linux.")
-
-    os.system(openCommand)
-
-def handleSingleArticle(vaultName, vaultPath, profileName, articleSource, articleURL):
-
-    openURILocally(articleURL)
-
-    # Load the profile for the article
-    currentProfile = json.loads(getProfiles(profileName))
-
-    # Gather the needed information from the article
-    articleDetails, articleContent, articleClearText = OSINTextract.extractAllDetails(currentProfile, articleSource)
-
-    # Generate the tags
-    articleTags = OSINTtext.generateTags(OSINTtext.cleanText(articleClearText))
-
-    # Create the markdown file
-    MDFileName = OSINTfiles.createMDFile(currentProfile['source']['name'], articleURL, articleDetails, articleContent, articleTags)
-    os.rename(str(Path(MDFileName).resolve()) + ".md", str(Path(vaultPath + MDFileName)) + ".md")
 
 
 def main():
     checkIfObsidianDetailsSet()
     profile = input("Which profile do you want to test? ")
     articleURLLists = OSINTscraping.gatherArticleURLs([getProfiles(profile)])
-
-    for URLlist in articleURLLists:
-        currentProfile = URLlist.pop(0)
-        for url in URLlist:
-            OSINTmisc.printDebug("Scraping: " + url)
-            articleSource = OSINTscraping.scrapePageDynamic(url)
-            handleSingleArticle(obsidianVault, vaultPath, currentProfile, articleSource, url)
+    OGTagCollection = OSINTtags.collectAllOGTags(articleURLLists)[profile]
+    for articleCollection in OGTagCollection:
+        os.system(f"firefox {articleCollection['url']}")
+    OGTagCollection.insert(0, profile)
+    scrapeUsingProfile(OGTagCollection, articlePath=vaultPath)
 
 if __name__ == "__main__":
     main()
