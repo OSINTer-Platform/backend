7f944c874c2fbe5f23dce75a4e6aa53cfc3a2ace
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-27 08:32:44 +0100

    Add progress counter when scraping prior zdnet articles

--- scripts/scrapePriorZdnetArticles.py
+++ scripts/scrapePriorZdnetArticles.py
@@ -3,6 +3,8 @@
 # The profiles mapping the different websites are in json format
 import json
 
+import os
+
 debugMessages = True
 
 from OSINTmodules.OSINTprofiles import getProfiles
@@ -13,13 +15,22 @@ from scripts.scrapeAndStore import scrapeUsingProfile
 def main():
     zdnetProfile = json.loads(getProfiles("zdnet"))
 
-    for i in range(0, 3301):
+    if os.path.isfile("./progress.txt"):
+        with open("./progress.txt", "r") as file:
+            progressCounter = int(file.read())
+    else:
+        progressCounter = 0
+
+    for i in range(progressCounter, 3301):
+        print(i)
+        with open("./progress.txt", "w") as file:
+            file.write(str(i))
         OSINTmisc.printDebug("Scraping page {}.".format(str(i)))
         articleURLCollection = [ OSINTscraping.scrapeArticleURLs("https://www.zdnet.com/", "https://www.zdnet.com/topic/security/{}/".format(str(i)), zdnetProfile["source"]["scrapingTargets"], "zdnet")]
 
         OGTagCollection = OSINTtags.collectAllOGTags(articleURLCollection)["zdnet"]
 
-        scrapeUsingProfile(OGTagCollection, "zdnet", articlePath="./ZDNetArticles")
+        scrapeUsingProfile(OGTagCollection, "zdnet", articlePath="./ZDNetArticles/")
 
 if __name__ == "__main__":
     main()
