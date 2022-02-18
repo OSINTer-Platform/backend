85ee7d3c02773da46e6148e0c16f56ad1be8e431
bertmad3400 <skrivtilbertram@gmail.com>
2022-02-19 00:45:26 +0100

    Refactor main script

--- main.py
+++ main.py
@@ -3,38 +3,46 @@
 import scripts
 from scripts import *
 
-scriptNames = [("initiateScrapingBackend", "Intiate the OSINTer backend"), ("profileTester", "Test a profile"), ("scrapeAndStore", "Scrape articles based on available profiles"), ("scrapePriorZdnetArticles", "Scrape old ZDNet articles"), ("verifyKeywordFiles", "Verify the available keyword files")]
+def selectScript(scriptNames):
+    print("Which script do you want ro run?")
 
-print("Which script do want ro run?")
+    for i,script in enumerate(scriptNames):
+        print(f"{str(i)}. {script[1]}")
 
-for i,script in enumerate(scriptNames):
-    print(f"{str(i)}. {script[1]}")
+    try:
+        scriptNumber = int(input("Write a number: "))
+    except:
+        print("It doesn't look like you entered a number.")
+        exit()
 
-try:
-    scriptNumber = int(input("Write a number: "))
-except:
-    print("It doesn't look like you entered a number.")
-    exit()
+    try:
+        return scriptNames[scriptNumber][0]
+    except IndexError:
+        print(f"The number you entered doesn't correspond to a script, it should be between 0 and {len(scriptNames) - 1}")
+        exit()
 
-try:
-    scriptName = scriptNames[scriptNumber][0]
-except IndexError:
-    print(f"The number you entered doesn't correspond to a script, it should be between 0 and {len(scriptNames) - 1}")
-    exit()
+def main():
+    scriptNames = [("initiateScrapingBackend", "Intiate the OSINTer backend"), ("profileTester", "Test a profile"), ("scrapeAndStore", "Scrape articles based on available profiles"), ("scrapePriorZdnetArticles", "Scrape old ZDNet articles"), ("verifyKeywordFiles", "Verify the available keyword files")]
 
-if scriptName == "profileTester":
-    from OSINTmodules import OSINTprofiles
-    profileList = OSINTprofiles.getProfiles(justNames = True)
+    scriptName = selectScript(scriptNames)
 
-    print("Available profiles:")
+    if scriptName == "profileTester":
+        from OSINTmodules import OSINTprofiles
+        profileList = OSINTprofiles.getProfiles(justNames = True)
 
-    for i, profileName in enumerate(profileList):
-        print(f"{str(i)}: {profileName}")
+        print("Available profiles:")
 
-    profile = profileList[int(input("Which profile do you want to test? "))]
+        for i, profileName in enumerate(profileList):
+            print(f"{str(i)}: {profileName}")
 
-    url = input("Enter specific URL or leave blank for scraping 10 urls by itself: ")
+        profile = profileList[int(input("Which profile do you want to test? "))]
 
-    scripts.profileTester.main(profile, url)
+        url = input("Enter specific URL or leave blank for scraping 10 urls by itself: ")
 
-eval(f"scripts.{scriptName}.main()")
+        scripts.profileTester.main(profile, url)
+        exit()
+
+    eval(f"scripts.{scriptName}.main()")
+
+if __name__ == "__main__":
+    main()
