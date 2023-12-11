6db52cb8c424f8e711982a39911887e37b836662
bertmad3400 <skrivtilbertram@gmail.com>
2023-12-11 11:33:17 +0000

    Add functionality for scraping old articles

--- scripts/scraping/__init__.py
+++ scripts/scraping/__init__.py
@@ -1,6 +1,14 @@
 import logging
+from modules.profiles import get_profile
 
-from .articles import scrape_articles
+from scripts.profile_tester import calculate_profile, get_profile_prompt
+from scripts.scraping.articles import (
+    gather_profile_urls,
+    scrape_articles,
+    scrape_using_profile,
+)
+from scripts import config_options
+
 import typer
 
 
@@ -20,3 +28,36 @@ def main() -> None:
                 f'Critical error prevented running the "{scraping_function.__name__}". Error: {e}',
                 exc_info=True,
             )
+
+
+@app.command()
+def timetravel(
+    profile: str = typer.Option(
+        ..., prompt=get_profile_prompt(), callback=calculate_profile
+    ),
+    url: str = typer.Option(prompt="Enter URL with % for substitution of page number"),
+    start_page: int = 2,
+    batch_size: int = 5,
+) -> None:
+    if not "%" in url:
+        raise Exception("Need a % for substituting the page number")
+
+    current_profile = get_profile(profile)
+
+    while True:
+        logger.info(f"Scraping page {start_page} to {start_page + batch_size}")
+
+        frontpage_urls = [
+            url.replace("%", str(i)) for i in range(start_page, start_page + batch_size)
+        ]
+
+        urls = gather_profile_urls(current_profile, 1000, frontpage_urls)
+
+        logger.debug(
+            "Removing those articles that have already been stored in the database"
+        )
+
+        filtered_urls = config_options.es_article_client.filter_document_list(urls)
+        scrape_using_profile(filtered_urls, profile)
+
+        start_page += batch_size
